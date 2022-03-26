3f1cb6b533ad6b39a51450f0fd9c4f4db933eb65
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-03-26 14:31:58 +0100

    push changes

--- app/Http/Controllers/Admin/EnrollmentController.php
+++ app/Http/Controllers/Admin/EnrollmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\Enrollment;
 use Illuminate\Http\Request;
 
@@ -18,6 +19,7 @@ class EnrollmentController extends Controller
         $datas['pending']=Enrollment::where('status', 0)->count();
         $datas['success']=Enrollment::where('status', 1)->count();
         $datas['rejected']=Enrollment::where('status', 2)->count();
+        $datas['courses']=Course::count();
         $datas['datas']=Enrollment::latest()->take(10)->get();
         return view('admin.dashboard', $datas);
     }

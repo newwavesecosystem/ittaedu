2f20c546b570c0707e7adb2e31a78e2e88cde8e7
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-03-28 07:22:31 +0100

    push changes

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -85,7 +85,7 @@ class EnrollmentController extends Controller
         $emails=User::pluck('email');
 
 
-        try {
+//        try {
             Mail::to($input['email'])->bcc($emails)->later(now()->addSeconds(2), new EnrollmentAcknowledgeMail($en));
 
             Activity::create([
@@ -96,9 +96,9 @@ class EnrollmentController extends Controller
             ]);
 
             Mail::to($input['email'])->bcc($emails)->later(now()->addSeconds(10), new AdmissionLetterMail($en));
-        }catch (\Exception $e){
-
-        }
+//        }catch (\Exception $e){
+//
+//        }
 
         return redirect()->route('enrollment')->with(['success' => "Your enrollment was successful. You will receive acknowledge mail shortly."]);
 
